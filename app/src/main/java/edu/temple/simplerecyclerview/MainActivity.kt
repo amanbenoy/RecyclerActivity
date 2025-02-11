ff29b4e7ea2a53033da169de7d059c9906a94678
amanbenoy <tun52299@temple.edu>
2025-02-11 12:11:06 -0500

    Complete NumberDisplayAdapter implementation and finished recylerView in MainActivity

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -9,12 +10,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO Step 1: Reference RecyclerView object
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
-        //TODO Step 2: Provide a LayoutManager
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
-
-        //TODO Step 4: Provide a RecyclerView.Adapter
+        val numbers = Array(100){i -> (i + 3)}
+        recyclerView.adapter = NumberDisplayAdapter(numbers)
     }
 }
